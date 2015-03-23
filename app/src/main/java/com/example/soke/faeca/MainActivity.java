672f5df6fdcf8466fb0de40c6728c47f00a126ad
Carlos Martínez Vellido <elpochito@hotmail.es>
2015-03-23 09:44:36 +0100

    que pasa paco

--- app/src/main/java/com/example/soke/faeca/MainActivity.java
+++ app/src/main/java/com/example/soke/faeca/MainActivity.java
@@ -28,8 +28,8 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Parse.enableLocalDatastore(this);
-        //Parse.initialize(this,"IGPp8uUXyGziD2kGBcLPhfzk5KqYyliY3gzjH3RR","xb0pfIjxiZgZhMgNt93b51J00HFOQTrUWe4NjJof");
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this,"IGPp8uUXyGziD2kGBcLPhfzk5KqYyliY3gzjH3RR","xb0pfIjxiZgZhMgNt93b51J00HFOQTrUWe4NjJof");
 
         ParsePush.subscribeInBackground("", new SaveCallback() {
             @Override
