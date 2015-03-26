5a12b5ba7052186dba29d9e769edf2b624d7d272
Carlos Martínez Vellido <elpochito@hotmail.es>
2015-03-26 11:12:26 +0100

    update

--- app/src/main/java/com/example/soke/faeca/EnviarA.java
+++ app/src/main/java/com/example/soke/faeca/EnviarA.java
@@ -2,7 +2,6 @@ package com.example.soke.faeca;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -29,54 +28,39 @@ public class EnviarA extends ActionBarActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_enviar);
-        selectorUsuarios=(Spinner) findViewById(R.id.usuarios);
-        selectorGrupos=(Spinner) findViewById(R.id.grupos);
-        mensaje=getIntent().getStringExtra("mensaje");
-
-        ParseQuery query = ParseInstallation.getQuery();
-        //
-        //
-        //
-        //
-        //
-        //Me quedo trasteando el query para que funcione como el del metodo Enviar!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //Busco los canales a los que estoy suscrito
-        List<String> subscribedChannels = ParseInstallation.getCurrentInstallation().getList("channels");
-        LinkedList<String> canales=new LinkedList<>();
-        for(int i=0; i<subscribedChannels.size(); i++)
-            canales.add(subscribedChannels.get(i));
-
-        //Lleno el Spinner con los canales a los que estoy suscrito
-        ArrayAdapter<String> grupos=new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, canales);
-        grupos.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        selectorGrupos=(Spinner) findViewById(R.id.grupos);
-        selectorGrupos.setAdapter(grupos);
-
 
         //Busco todos los usuarios de la aplicacion para poder mandarles push
-
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("_User");
         final LinkedList<String> usuarios=new LinkedList<>();
         query.findInBackground(new FindCallback<ParseObject>() {
             public void done(List<ParseObject> users, ParseException e) {
                 if (e == null) {
                     for (int i = 0; i < users.size(); i++) {
-                        usuarios.add(users.get(i).get("user").toString());
-                        Log.d("$$$$$$$$$$$","usuario: "+usuarios.get(i).toString());
+                        usuarios.add(users.get(i).get("username").toString());
                     }
                 } else {
                     e.printStackTrace();
                 }
             }
         });
+
+        //Busco los canales a los que estoy suscrito
+        List<String> subscribedChannels = ParseInstallation.getCurrentInstallation().getList("channels");
+        LinkedList<String> canales=new LinkedList<>();
+        for(int i=0; i<subscribedChannels.size(); i++)
+            canales.add(subscribedChannels.get(i));
+
+        setContentView(R.layout.activity_enviar);
+        selectorUsuarios=(Spinner) findViewById(R.id.usuarios);
+        selectorGrupos=(Spinner) findViewById(R.id.grupos);
+        mensaje=getIntent().getStringExtra("mensaje");
+
+
+        //Lleno el Spinner con los canales a los que estoy suscrito
+        ArrayAdapter<String> grupos=new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, canales);
+        grupos.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        selectorGrupos=(Spinner) findViewById(R.id.grupos);
+        selectorGrupos.setAdapter(grupos);
 
         //Lleno el Spinner con los usuarios registrados
         ArrayAdapter<String> adapterUsuarios=new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, usuarios);
