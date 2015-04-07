6802d543881a735a8699b6becae133676040c097
Carlos Martínez Vellido <elpochito@hotmail.es>
2015-04-07 11:41:25 +0200

    paco

--- app/src/main/java/com/example/soke/faeca/MenuPrincipal.java
+++ app/src/main/java/com/example/soke/faeca/MenuPrincipal.java
@@ -140,7 +140,7 @@ public class MenuPrincipal extends Activity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
+        // automatically handle clicks on the Home/Up boton_enviar_a, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
