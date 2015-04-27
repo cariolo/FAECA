9f4ddd9167ef414fc1be17696d9c7cc5dca8f27b
Carlos Martínez Vellido <elpochito@hotmail.es>
2015-04-27 11:47:59 +0200

    update

--- app/src/main/java/com/example/soke/faeca/ReunionGPS.java
+++ app/src/main/java/com/example/soke/faeca/ReunionGPS.java
@@ -1,6 +1,8 @@
 package com.example.soke.faeca;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
@@ -20,11 +22,12 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class ReunionGPS extends FragmentActivity implements LocationListener {
+public class ReunionGPS extends FragmentActivity implements LocationListener{
 
     CameraPosition cameraPosition = null;
     CameraUpdate camera = null;
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    static LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +35,15 @@ public class ReunionGPS extends FragmentActivity implements LocationListener {
         setContentView(R.layout.activity_reunion_gps);
         setTitle("Mapa");
 
-        String contenedor = null;
         int sdk_version = Build.VERSION.SDK_INT;
-        contenedor = String.valueOf(sdk_version);
+        String contenedor = String.valueOf(sdk_version);
+
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if ( !locationManager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
+            buildAlertMessageNoGps();
+        }
 
         if (contenedor.equals("21")) {
             mapIfLollipop();
@@ -46,7 +55,9 @@ public class ReunionGPS extends FragmentActivity implements LocationListener {
         mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
             @Override
             public void onMapLongClick(LatLng latLng) {
+                mMap.addMarker(new MarkerOptions().position(latLng));
                 String latitud = String.valueOf(latLng.latitude), longitud = String.valueOf(latLng.longitude);
+
                 Toast.makeText(getApplicationContext(), latitud + " " + longitud, Toast.LENGTH_LONG).show();
             }
         });
@@ -54,27 +65,31 @@ public class ReunionGPS extends FragmentActivity implements LocationListener {
 
     }
 
+    private void buildAlertMessageNoGps(){
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Parece que tu GPS esta desactivado. Activarlo mejora la precision de la localizacion.\n¿Desea activarlo?")
+                .setCancelable(false)
+                .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,ReunionGPS.this);
+                        startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,0,0,ReunionGPS.this);
+                        dialog.cancel();
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
         setUpMapIfNeeded();
     }
-
-    /**
-     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
-     * installed) and the map has not already been instantiated.. This will ensure that we only ever
-     * call {@link #setUpMap()} once when {@link #mMap} is not null.
-     * <p/>
-     * If it isn't installed {@link SupportMapFragment} (and
-     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
-     * install/update the Google Play services APK on their device.
-     * <p/>
-     * A user can return to this FragmentActivity after following the prompt and correctly
-     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
-     * have been completely destroyed during this process (it is likely that it would only be
-     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
-     * method in {@link #onResume()} to guarantee that it will be called.
-     */
     private void setUpMapIfNeeded() {
         // Do a null check to confirm that we have not already instantiated the map.
         if (mMap == null) {
@@ -89,14 +104,8 @@ public class ReunionGPS extends FragmentActivity implements LocationListener {
         }
     }
 
-    /**
-     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
-     * just add a marker near Africa.
-     * <p/>
-     * This should only be called once and when we are sure that {@link #mMap} is not null.
-     */
 
-    private void mapIfLollipop() {
+    private void mapIfLollipop(){
         Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
                 Uri.parse("https://www.google.com/maps/place/37%C2%B009'53.9%22N+3%C2%B036'27.6%22W/@37.164972,-3.60766,19z/data=!3m1!4b1!4m2!3m1!1s0x0:0x0"));
         startActivity(intent);
@@ -109,16 +118,13 @@ public class ReunionGPS extends FragmentActivity implements LocationListener {
                 .title("Cooperativas Agro-Alimentarias")
                 .icon(BitmapDescriptorFactory.fromResource(R.mipmap.reunion)));
 
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
-        ////////////////////////////////////////////////////ME QUEDO AÑADIENDO LOS LSITENERS PARA LOS MARCADORES
 
     }
 
     @Override
     public void onLocationChanged(Location location) {
-        cameraPosition = new CameraPosition(new LatLng(location.getLatitude(), location.getLongitude()), 15, 0, 0);
-        camera = CameraUpdateFactory.newCameraPosition(cameraPosition);
+        cameraPosition=new CameraPosition(new LatLng(location.getLatitude(), location.getLongitude()), 15, 0, 0);
+        camera= CameraUpdateFactory.newCameraPosition(cameraPosition);
         mMap.animateCamera(camera);
     }
 
